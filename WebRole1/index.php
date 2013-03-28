3783f1ac75a0460e49ca6a130b2516b316ca47a3
Ivan Mosiev <i.k.mosev@gmail.com>
2013-03-28 02:13:56 +0200

    Added website example into web role

--- WebRole1/index.php
+++ WebRole1/index.php
@@ -1,3 +1,27 @@
 <?php
-phpinfo();
-?>
+include_once('vendor/autoload.php');
+
+$app = new Silex\Application();
+
+$app->get('/blobs/', 'Demos\Blobs::index');
+$app->post('/blobs/upload', 'Demos\Blobs::upload');
+$app->get('/blobs/delete/{name}', 'Demos\Blobs::delete')
+    ->assert('name', '.+');
+
+$app->get('/mysql/', 'Demos\MySQL::index');
+$app->post('/mysql/register', 'Demos\MySQL::register');
+
+$app->get('/tables/', 'Demos\Tables::index');
+$app->post('/tables/save', 'Demos\Tables::save');
+
+$app->get('/queues/', 'Demos\Queues::index');
+$app->post('/queues/add', 'Demos\Queues::add');
+$app->post('/queues/list', 'Demos\Queues::dequeue');
+
+
+$app->get('/info/', function() { phpinfo(); });
+$app->get('/', 'Demos\Dashboard::index');
+
+$app['debug'] = true;
+
+$app->run();
